7b12ae9793d7429a6c7bd0fbdfbecc1336573398
Kristina Adigamova <kriwowak@yandex.ru>
2022-11-21 19:29:55 +0500

    сделано

--- src/Human.java
+++ src/Human.java
@@ -2,16 +2,18 @@ public class Human {
     int yearOfBirth;
     String name;
     String town;
+    String development;
 
-    public Human(int yearOfBirth, String name, String town) {
+    public Human(int yearOfBirth, String name, String town, String development) {
         this.yearOfBirth = yearOfBirth;
         this.name = name;
         this.town = town;
+        this.development = development;
     }
 
-        void man() {
-            System.out.println("Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился в" + yearOfBirth +
-                    " году. Будем знакомы!");
+    public String toString() {
+            return "Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился в" + yearOfBirth +
+                    " году. Работаю " + development + ". Будем знакомы!";
 
         }
 
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
-    public static void main (){
-        Human maksim = new Human(1988, "Максим", "Минск");
+    public static void main (String[] args){
+        Human maksim = new Human(1988, "Максим", "Минск", "бренд-менеджером");
+        Human anna = new Human(1993, "Анна", "Москва", "методистом образовательных программ");
+        Human katy = new Human(1992, "Катя", "Калининград", "продакт-менеджером");
+        Human artem = new Human(1995, "Артем", "Москва", "директором по развитию бизнеса");
+        System.out.println(maksim);
+        System.out.println(anna);
+        System.out.println(katy);
+        System.out.println(artem);
     }
 }
